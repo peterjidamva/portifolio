2ab716134750d0eb2037c000db36260f13a6dcc0
jidamva <jidamvap@gmail.com>
2023-03-25 13:46:27 +0300

    it is getting better

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import eastc from "../../Assets/Projects/eastc.png";
+import eastc from "../../Assets/Projects/shortcourse.png";
 import chat from "../../Assets/Projects/chat.png";
 import web3 from "../../Assets/Projects/web3.png";
-import dex from "../../Assets/Projects/dex.png";
+import dex from "../../Assets/Projects/6.png";
 import goat from "../../Assets/Projects/goat.png";
+import stei from "../../Assets/Projects/2.png";
 
 function Projects() {
   return (
@@ -20,6 +21,35 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={eastc}
+              isBlog={false}
+              title="Short Course Application"
+              description="Project for Appliying for shortcourses at EAST AFRICAN STATISTICAL TRAINING CENTRE."
+              link="https://shortcourse.eastc.ac.tz/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={dex}
+              isBlog={false}
+              title="Nft Puzzle"
+              description="A puzzle Game of Your Favourite Nfts made with flutter."
+              link="https://play.google.com/store/apps/details?id=app.jidamva.puzzle"
+            />
+          </Col>
+         
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={stei}
+              isBlog={false}
+              title="STEI"
+              description="A website for listing and selling real estate."
+              link="stei.co.tz"
+            />
+          </Col>
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={web3}
@@ -51,25 +81,9 @@ function Projects() {
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={eastc}
-              isBlog={false}
-              title="Short Course Application"
-              description="Project for Appliying for shortcourses at EAST AFRICAN STATISTICAL TRAINING CENTRE."
-              link="https://shortcourse.eastc.ac.tz/"
-            />
-          </Col>
+         
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={dex}
-              isBlog={false}
-              title="Nft Puzzle"
-              description="A puzzle Game of Your Favourite Nfts made with flutter."
-              link="https://github.com/peterjidamva/nft-puzzle"
-            />
-          </Col>
+         
         </Row>
       </Container>
     </Container>
